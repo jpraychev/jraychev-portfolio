59383ecf7d82f698961c8b2fc21d20eb5389b931
Jordan Raychev <jpraychev@gmail.com>
2022-09-12 17:00:53 +0300

    added gunicorn config

---
+++ deployment/gunicorn-conf.py
@@ -0,0 +1,13 @@
+import multiprocessing
+from pathlib import Path
+
+# Dynamically computed values
+base_dir = str(Path(__file__).parent.parent.resolve().joinpath('src'))
+worker_nodes = multiprocessing.cpu_count() * 2 + 1
+
+chdir = base_dir
+bind = "0.0.0.0:8080"
+workers = worker_nodes
+accesslog = 'logs/access.log'
+errorlog =  'logs/debug.log'
+loglevel  = 'DEBUG'
