c9ce13a7a4ff4daeb5256ab550cd3ca336ac569a
Jordan Raychev <jpraychev@gmail.com>
2022-09-27 06:31:08 +0300

    added debug cmds inside docker

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,6 @@
 gunicorn -c /opt/portfolio/deployment/gunicorn-conf.py app:app --daemon
 cd /opt/portfolio/tests/
 pytest -s
-cat /opt/portfolio/src/logs/debug.log
+ls -la /opt/portfolio/tests
+ls -la /opt/portfolio/tests/drivers
+whoami
