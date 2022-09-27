84501af099a3524f23e46bf145cd425e1e00eb6f
Jordan Raychev <jpraychev@gmail.com>
2022-09-27 06:35:59 +0300

    geckodriver +x mode

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 gunicorn -c /opt/portfolio/deployment/gunicorn-conf.py app:app --daemon
 cd /opt/portfolio/tests/
+chmod +x /opt/portfolio/tests/drivers/geckodriver
 pytest -s
 ls -la /opt/portfolio/tests
 ls -la /opt/portfolio/tests/drivers
